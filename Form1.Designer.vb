1eafd3854d6458e2e9b496757ba2f0de43769321
Nicholas Rutledge <njrutledge2000@gmail.com>
2024-01-16 15:55:06 -0500

    pop-up window created with timer for close

---
+++ Form1.Designer.vb
@@ -0,0 +1,80 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnClose = New Button()
+        btnStartTimer = New Button()
+        lblText = New Label()
+        SuspendLayout()
+        ' 
+        ' btnClose
+        ' 
+        btnClose.Enabled = False
+        btnClose.Font = New Font("Segoe UI", 24F)
+        btnClose.Location = New Point(238, 164)
+        btnClose.Name = "btnClose"
+        btnClose.Size = New Size(189, 54)
+        btnClose.TabIndex = 1
+        btnClose.Text = "<--"
+        btnClose.UseVisualStyleBackColor = True
+        ' 
+        ' btnStartTimer
+        ' 
+        btnStartTimer.Font = New Font("Segoe UI", 24F)
+        btnStartTimer.Location = New Point(12, 164)
+        btnStartTimer.Name = "btnStartTimer"
+        btnStartTimer.Size = New Size(189, 54)
+        btnStartTimer.TabIndex = 0
+        btnStartTimer.Text = "Start timer"
+        btnStartTimer.UseVisualStyleBackColor = True
+        ' 
+        ' lblText
+        ' 
+        lblText.Font = New Font("Segoe UI", 24F)
+        lblText.Location = New Point(12, 32)
+        lblText.Name = "lblText"
+        lblText.Size = New Size(415, 90)
+        lblText.TabIndex = 2
+        lblText.Text = "Its time for your 20 second check-in!"
+        lblText.TextAlign = ContentAlignment.TopCenter
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        AutoSize = True
+        BackColor = SystemColors.Window
+        ClientSize = New Size(439, 230)
+        ControlBox = False
+        Controls.Add(lblText)
+        Controls.Add(btnClose)
+        Controls.Add(btnStartTimer)
+        Name = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents btnClose As Button
+    Friend WithEvents btnStartTimer As Button
+    Friend WithEvents lblText As Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,82 @@
+﻿
+Imports System.Threading.Tasks
+Public Class Form1
+    Private WithEvents countdownTimer As New Timer()
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+
+        Me.TopMost = True
+
+        'SchedulePopupAt4PM()
+    End Sub
+
+    Private Sub btnStartTimer_Click(sender As Object, e As EventArgs) Handles btnStartTimer.Click
+        btnStartTimer.Enabled = False
+        countdownTimer.Interval = 1000
+        AddHandler countdownTimer.Tick, AddressOf UpdateCountdown
+        remainingTime = 20
+        countdownTimer.Enabled = True
+        btnClose.Text = $"{remainingTime} sec remaining"
+
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        btnClose.Enabled = False
+
+        Close()
+    End Sub
+
+    Private remainingTime As Integer = 20
+
+    Public Sub UpdateCountdown(sender As Object, e As EventArgs)
+        If remainingTime > 0 Then
+            remainingTime -= 1
+            btnClose.Text = $"{remainingTime} sec remaining"
+        Else
+            btnClose.Enabled = True
+            countdownTimer.Stop()
+            btnClose.Text = "Close"
+            countdownTimer.Enabled = False
+        End If
+    End Sub
+
+    'Private Sub SchedulePopupAt4PM()
+    ' Schedule a task using Task Scheduler to run your app every day at 3 PM
+    'Dim taskScheduler = New Microsoft.Win32.TaskScheduler()
+    '   taskScheduler.Connect()
+    '
+    'Dim taskDef As ITaskDefinition = taskScheduler.NewTask(0)
+    '    taskDef.RegistrationInfo.Description = "Popup App at 3 PM"
+    '    taskDef.Principal.UserId = "NT AUTHORITY\SYSTEM"
+
+    'Dim trigger As ITrigger = taskDef.Triggers.Create(_
+    '        TaskTriggerType.Daily,
+    '        1,
+    '0,
+    '0,
+    '15,
+    '0)
+    '
+    '   Dim execAction As IExecAction = CType(taskDef.Actions.Create(TaskActionType.Execute), IExecAction)
+    '      execAction.Path = Application.ExecutablePath
+    '
+    'Dim rootFolder As ITaskFolder = taskScheduler.GetFolder("\")
+    '   rootFolder.RegisterTaskDefinition(
+    '      "PopupAppTask",
+    '     taskDef,
+    'CInt(TaskCreation.CreateOrUpdate),
+    '        Nothing,
+    '        Nothing,
+    '        TaskLogonType.InteractiveToken,
+    '        Nothing)
+    'End Sub
+
+
+
+End Class
